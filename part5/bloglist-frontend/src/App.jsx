1876c024bc1ab9ecdff6ba26ed7fbde01edc835b
IiroP <iiro.poutiainen@gmail.com>
2025-08-31 15:57:51 +0300

    5.3 and 5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -8,7 +8,11 @@ const App = () => {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [user, setUser] = useState(null)
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [message, setMessage] = useState({})
+
+  const [blogTitle, setBlogTitle] = useState("")
+  const [blogAuthor, setBlogAuthor] = useState("")
+  const [blogUrl, setBlogUrl] = useState("")
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -16,18 +20,41 @@ const App = () => {
     try {
       const user = await login.login({ username, password })
       window.localStorage.setItem("loggedInUser", JSON.stringify(user))
+      blogService.setToken(user.token)
       setUser(user)
       setUsername("")
       setPassword("")
     } catch (exception) {
       console.error(exception)
-      setErrorMessage("Login failed, check username and password")
+      showMessage("error", "Login failed, check username and password")
     }
   }
 
   const logout = () => {
     window.localStorage.removeItem("loggedInUser")
+    blogService.setToken(null)
     setUser(null)
+  }
+
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+
+    try {
+      const newBlog = {
+        title: blogTitle,
+        author: blogAuthor,
+        url: blogUrl
+      }
+      const response = await blogService.create(newBlog)
+      setBlogs(blogs.concat(response))
+      setBlogTitle("")
+      setBlogAuthor("")
+      setBlogUrl("")
+      showMessage("success", `A new blog "${response.title}" by ${response.author} added`)
+    } catch (exception) {
+      console.error(exception)
+      showMessage("error", "Blog creation failed, check the input")
+    }
   }
 
   useEffect(() => {
@@ -44,21 +71,39 @@ const App = () => {
     }
   }, [])
 
+  const showMessage = (type, content) => {
+    setMessage({ type, content })
+    setTimeout(() => {
+      setMessage({})
+    }, 5000)
+  }
+
+  const msgBox = () => {
+    if (message.content) {
+      return (
+        <div className={`message ${message.type}`}>
+          {message.content}
+        </div>
+      )
+    }
+  }
+
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
+        {msgBox()}
         <form onSubmit={handleLogin}>
           <div>
             <label>
               Username
-              <input type="text" name="username" onChange={({ target }) => setUsername(target.value)} />
+              <input type="text" name="username" value={username} onChange={({ target }) => setUsername(target.value)} />
             </label>
           </div>
           <div>
             <label>
               Password
-              <input type="password" name="password" onChange={({ target }) => setPassword(target.value)} />
+              <input type="password" name="password" value={password} onChange={({ target }) => setPassword(target.value)} />
             </label>
           </div>
           <div>
@@ -72,7 +117,35 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      {msgBox()}
       <p>{user.name} logged in <button onClick={logout}>Logout</button></p>
+
+      <h3>Create new blog</h3>
+      <form onSubmit={handleCreateBlog}>
+        <div>
+          <label>
+            Title:
+            <input type="text" name="title" value={blogTitle} onChange={({ target }) => setBlogTitle(target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Author:
+            <input type="text" name="author" value={blogAuthor} onChange={({ target }) => setBlogAuthor(target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            URL:
+            <input type="text" name="url" value={blogUrl} onChange={({ target }) => setBlogUrl(target.value)} />
+          </label>
+        </div>
+        <div>
+          <button type="submit">Create blog</button>
+        </div>
+      </form>
+
+      <h3>Current blogs</h3>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
