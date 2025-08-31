bade5d3c292e8009dbbd98238fe3a06f0dfe91e7
IiroP <iiro.poutiainen@gmail.com>
2025-08-31 17:26:20 +0300

    5.7 - 5.9

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+import blogService from "../services/blogs";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [open, setOpen] = useState(false);
+  const [likes, setLikes] = useState(blog.likes);
+
+  const handleLike = async () => {
+    try {
+      await blogService.like(blog.id);
+      setLikes(likes + 1);
+    } catch (error) {
+      console.error("Error liking blog:", error);
+    }
+  };
+
+  return (
+    <div style={{ padding: 5 }}>
+      {blog.title} {blog.author}
+      <button onClick={() => setOpen(!open)}>{open ? "Hide" : "View"}</button>
+      {open && (
+        <div>
+          <span>{blog.url}</span>
+          <br />
+          <span>
+            Likes: {likes} <button onClick={handleLike}>Like</button>
+          </span>
+          <br />
+          <span>Added by {blog.user.name}</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,23 +1,44 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from "axios";
+const baseUrl = "/api/blogs";
 
-let token = null
+let token = null;
 
-const setToken = newToken => {
-  token = `Bearer ${newToken}`
-}
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const getAll = async () => {
+  const request = axios.get(baseUrl);
+  const response = await request;
+  return response.data;
+};
 
-const create = async newBlog => {
+const create = async (newBlog) => {
   const config = {
     headers: { Authorization: token },
-  }
-  const response = await axios.post(baseUrl, newBlog, config)
-  return response.data
-}
+  };
+  const response = await axios.post(baseUrl, newBlog, config);
+  return response.data;
+};
 
-export default { getAll, setToken, create }
+const like = async (id) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const blogs = await getAll();
+  const blog = blogs.find((b) => b.id === id);
+  if (!blog) {
+    throw new Error("blog not found");
+  }
+  const updatedBlog = {
+    user: blog.user.id,
+    likes: blog.likes + 1,
+    author: blog.author,
+    title: blog.title,
+    url: blog.url,
+  };
+  const response = await axios.put(`${baseUrl}/${id}`, updatedBlog, config);
+  return response.data;
+};
+
+export default { getAll, setToken, create, like };
